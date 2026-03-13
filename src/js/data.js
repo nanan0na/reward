f7ff72d92d3c1590cfbbfbe1a9856bf920140523
nayoung-0 <na0@aisoul.co.kr>
2026-03-13 16:11:07 +0900

    stamp1

--- src/js/data.js
+++ src/js/data.js
@@ -2,17 +2,17 @@ export const stamps = [
   {
     id: 1,
     title: 'Pieles',
-    date: '2026.03.11',
+    date: '2026.03.12',
     thumbnail: '../images/mv1.png',
     image: '../images/detail.png'
   },
-  {
-    id: 2,
-    title: 'Movie 2',
-    date: '2026.03.12',
-    thumbnail: '../images/mv2.png',
-    image: '../images/mv2.png'
-  }
+  // {
+  //   id: 2,
+  //   title: 'Movie 2',
+  //   date: '2026.03.12',
+  //   thumbnail: '../images/mv2.png',
+  //   image: '../images/mv2.png'
+  // }
 ];
 
 export const GRID_COUNT = 50;
